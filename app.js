2ee832898d229d51b187ed8cac397da33f6bc0f9
Mihir Chaturvedi <plibither8@gmail.com>
2018-08-11 09:03:48 +0530

    ProjectBeta 2.0

--- app.js
+++ app.js
@@ -15,28 +15,23 @@ app.use(session({
     }
 }))
 
-//Setting up body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-//Setting public directory
 app.use(express.static(__dirname + '/public'));
 
-//Setting view engine
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views/pages');
 
 const home = require('./routes/index');
 app.use('/', home);
 
-//404
 app.use((res, req, next) => {
     next(err);
 });
 
-//Error Handler
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.redirect('./#/404');
